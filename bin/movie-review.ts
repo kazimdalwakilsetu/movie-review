1b480ddfdde7a9555e5584e49cf387b79bddb2ae
kazimdalwakil <20115749@setu.ie>
2026-03-23 17:12:20 +0000

    Initialised CDK app entry point for MovieReviewStack

--- bin/movie-review.ts
+++ bin/movie-review.ts
@@ -4,6 +4,7 @@ import { MovieReviewStack } from '../lib/movie-review-stack';
 
 const app = new cdk.App();
 new MovieReviewStack(app, 'MovieReviewStack', {
+  env: { region: "eu-west-1" },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
